d0ec844c7116d65e590849d8b18660f94caca77b
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-10-21 11:53:55 +0200

    change to env settings

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const firebase = require("firebase");
 const http = require('http').Server(app);
 const io = require('socket.io')(process.env.SOCKET_PORT);
 
-
 app.get('/', (req, res) => {
 	res.send('<h1>Hello world</h1>');
 });
