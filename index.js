5fe202591e98415de38aa1b2302d5c71600c53ec
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-10-21 11:33:57 +0200

    add express and socket io

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const io = require('socket.io')(80);
-const cfg = require('./config.json');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(3001);
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
