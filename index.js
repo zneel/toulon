4ff7549354d3a5c54301020231a58fdc2ac518ce
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-10-21 11:45:56 +0200

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const app = require('express')();
+const firebase = require("firebase");
 const http = require('http').Server(app);
 const io = require('socket.io')(3001);
+
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello world</h1>');
