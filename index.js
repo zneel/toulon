4d8f97d5285182f112e91d9c16a2a7fa9a3bcce6
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-10-21 11:50:33 +0200

    update

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const app = require('express')();
 const firebase = require("firebase");
 const http = require('http').Server(app);
-const io = require('socket.io')(3001);
+const io = require('socket.io')(process.env.SOCKET_PORT);
 
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, () => {
-	console.log('listening on *:3000');
+http.listen(process.env.HTTP_PORT, () => {
+	console.log(`listening on ${process.env.HTTP_PORT}`);
 });
