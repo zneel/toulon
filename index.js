8bc6530815a3cb6abad0676a820cc96ced0ebb54
Elliot Bouvier <elliotbouvier@protonmail.com>
2018-10-21 12:18:13 +0200

    init firebase

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const firebase = require("firebase");
 const http = require('http').Server(app);
 const io = require('socket.io')(process.env.SOCKET_PORT);
 
+firebase.initializeApp({
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	storageBucket: process.env.STORAGE_BUCKET,
+});
 app.get('/', (req, res) => {
 	res.send('<h1>Hello world</h1>');
 });
